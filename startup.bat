b099ca587cd012a5c3abbc57bf8bd123bd5a106e
GUO <quick803@gmail.com>
2015-02-24 02:14:37 -0700

    Simplify the project.

--- startup.bat
+++
@@ -1,6 +0,0 @@
-@echo off
-title WebRTC Server
-:start
-node server.js
-pause
-goto :start
--- static/example.js
+++
@@ -1,7 +0,0 @@
-function moduleDidLoad() {
-    //载入模块之后要做的事
-}
-
-function post(msg){
-common.naclModule.postMessage(msg);
-}
